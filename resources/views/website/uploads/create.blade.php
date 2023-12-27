4f23cab93eac7b0995e087bbe1cd1bdb9e547480
Moid Khan <mueedkhan766@gmail.com>
2023-12-27 11:53:12 +0500

    add: image preview

--- resources/views/website/uploads/create.blade.php
+++ resources/views/website/uploads/create.blade.php
@@ -1,4 +1,6 @@
 @extends('website.layout')
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+    integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 
 @section('content')
@@ -21,12 +23,13 @@
                             <div class="col-lg-12">
                                 <h5>Upload File</h5>
                                 <div class="row">
-                                    <div class="col-lg-12 col-md-12 col-sm-12">
+                                    <div class="col-lg-5 col-md-5 col-sm-5">
                                         <div class="checkout__form__input">
                                             <label for="upload_file">Upload File<span>* (Click here)</span></label>
                                             <p class="filename"></p>
                                             <input style="visibility: hidden" id="upload_file" name="upload_file"
                                                 type="file"
+                                                oninput="previewImg.src=window.URL.createObjectURL(this.files[0])"
                                                 class="form-control @error('upload_file') is-invalid @enderror"
                                                 name="upload_file" required autofocus>
                                             @error('upload_file')
@@ -35,6 +38,10 @@
                                             </span>
                                             @enderror
                                         </div>
+                                    </div>
+                                    <div class="col-7 justify-content-center d-flex">
+                                        <img id="previewImg" height="300px"  alt="">
+
                                     </div>
 
                                     <div class="col-lg-12">
